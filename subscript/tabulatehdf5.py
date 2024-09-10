444e7f42bc45a95db6dc9fe505d0f3a06891d38d
Charles Gannon <cgannon@ucmerced.edu>
2024-09-10 00:46:59 -0700

    Added tabulatehdf5.py

---
+++ subscript/tabulatehdf5.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import h5py
+from typing import Callable, Iterable
+import numpy as np
+import time
+from collections import UserDict
+from subscript.parameters import NodePropertyLabel
+
+class nodeProperties(UserDict):
+    _nodefilter = None
+ 
+    def __init__(self, d, nodefilter=None):
+        self._nodefilter = nodefilter 
+        out = super(nodeProperties,self).__init__()
+        self.data = d
+
+    def filter(self, nodefilter):
+        return nodeProperties(self,nodefilter)
+
+    def __getitem__(self, key): 
+        # Allow for providing a set of keys
+        if not isinstance(key, str):
+            return [self[_key] for _key in key] 
+
+        val = self.data[key]
+        if isinstance(val, np.ndarray): 
+            out = val
+        elif isinstance(val, h5py.Dataset):
+            _val = val[:]
+            self.data[key] = _val
+            out = _val
+        elif isinstance(val, Callable):
+            _val = val()
+            self.data[key] = _val
+            out = _val
+        else:
+            raise RuntimeError("Unrecognized Type") 
+
+        if self._nodefilter is None:
+            return out    
+        return out[self._nodefilter] 
+                
+def get_galacticus_outputs(galout:h5py.File)->np.ndarray[int]:
+    output_groups:h5py.Group = galout["Outputs"] 
+
+    outputs = np.zeros(len(output_groups), dtype=int)
+    for n,key in enumerate(output_groups.keys()):
+        outputs[n] = int(key[6:])
+    return np.sort(outputs)
+
+def get_custom_dsets(goutn:h5py.Group):
+    """Generates standar custom datasets to make data analysis easier"""    
+
+    # Total number of nodes
+    nodecount = np.sum(goutn["mergerTreeCount"][:]) 
+    # Node counts
+    counts    = goutn["mergerTreeCount"]
+    # Tree indexes
+    treenums  = goutn["mergerTreeIndex"]
+
+    nodedata  = goutn["nodeData"]
+    
+    return {
+        "custom_node_tree"            : lambda : np.concatenate([np.full(count,index) for (count,index) in zip(counts,treenums)]),
+        "custom_node_tree_outputorder": lambda : np.concatenate([np.full(count,i) for (i,count) in enumerate(counts)]),
+        "custom_id"                   : lambda : np.arange(nodecount),
+    }
+
+
+def tabulate_nodes(gout:h5py.File, key_index:int=-1, custom_dsets:Callable = None, **kwargs)->nodeProperties:
+    """Reads node propreties from a galacticus HDF5 file"""
+    outs = gout["Outputs"] 
+
+    _key_index = key_index
+    if key_index == -1:
+        _key_index = np.max(get_galacticus_outputs(gout))
+    
+    outn:h5py.Group = outs[f"Output{_key_index}"]
+    nd:h5py.Group     = outn["nodeData"]
+
+    # Total number of nodes in output can be obtained by summing this dataset
+    # Which contains the number of nodes per tree
+    nodecount = np.sum(outn["mergerTreeCount"][:]) 
+
+    nodedata = {}
+    for key, val in nd.items():
+        # Add to return dictionary if we have a dateset that matches the total number of nodes
+        if not isinstance(val, h5py.Dataset):
+            continue
+        if val.shape[0] != nodecount:
+            continue
+        # First entry in tuple marks this as a h5py dataset 
+        nodedata[key] = val
+
+    get_cdsets = get_custom_dsets if custom_dsets is None else custom_dsets
+ 
+    props = nodeProperties(get_cdsets(outn) | nodedata)
+
+    return props         
